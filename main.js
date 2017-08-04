0c3c74d294244d3863d5c97b53bb35afccce67f2
Pawel Psztyc <jarrodek@gmail.com>
2017-08-04 15:10:50 -0700

    Initial commit for the release

---
+++ main.js
@@ -0,0 +1,67 @@
+'use strict';
+
+const {Transport} = require('./lib/transport.js');
+const {GithubResolver} = require('./lib/github-resolver.js');
+const {GithubResolverOptions} = require('./lib/github-resolver-options.js');
+
+/**
+ * Copyright (C) Mulesoft.
+ * Shared under Apache 2.0 license
+ *
+ * @author Pawel Psztyc <pawel.psztyc@mulesoft.com>
+ */
+/**
+ * Gets the default options for the resolver class.
+ * It discovers `GITHUB_TOKEN` variable and if set it uses it to authorize the
+ * request.
+ *
+ * @return {GithubResolverOptions} Options object.
+ */
+function getResolverOptions() {
+  var token = process.env.GITHUB_TOKEN;
+  return new GithubResolverOptions({
+    token: token
+  });
+}
+
+/**
+ * Sorthand function to `GithubResolver#getLatestInfo()`.
+ *
+ * @return {Promise<Object>} A promise that resolves to a GitHub release info
+ * object.
+ */
+module.exports.latestInfo = function() {
+  const resolver = new GithubResolver(getResolverOptions());
+  return resolver.getLatestInfo();
+};
+/**
+ * Sorthand function to `GithubResolver#getTagInfo()`.
+ *
+ * @param {String} tag Release tag name
+ * @return {Promise} Resolved promise with an `Object` with release information.
+ */
+module.exports.tagInfo = function(tag) {
+  const resolver = new GithubResolver(getResolverOptions());
+  return resolver.getTagInfo(tag);
+};
+/**
+ * Sorthand function to `GithubResolver#getReleasesList()`.
+ *
+ * @return {Promise} Promise resolves to an array of releases information.
+ */
+module.exports.releasesInfo = function(tag) {
+  const resolver = new GithubResolver(getResolverOptions());
+  return resolver.getReleasesList(tag);
+};
+/**
+ * A library to get a resource from a secured location.
+ */
+module.exports.ApiConsoleTransport = Transport;
+/**
+ * A library to get an information about API console release from GitHub.
+ */
+module.exports.ApiConsoleGithubResolver = GithubResolver;
+/**
+ * Options object for the `ApiConsoleGithubResolver`
+ */
+module.exports.ApiConsoleGithubResolverOptions = GithubResolverOptions;
---
+++ test/transport.test.js
@@ -0,0 +1,73 @@
+'use strict';
+
+const {Transport} = require('../lib/transport.js');
+const assert = require('chai').assert;
+
+function getHeaders(extra) {
+  const token = process.env.GITHUB_TOKEN;
+  var headers = {
+    'user-agent': 'mulesoft-labs/api-console-github-resolver'
+  };
+  if (token) {
+    headers.authorization = 'token ' + token;
+  }
+  if (extra) {
+    headers = Object.assign(headers, extra);
+  }
+  return headers;
+}
+
+describe('Transport library', () => {
+  describe('get() JSON', () => {
+    const jsonUrl = 'https://api.github.com/repos/mulesoft/api-console/releases';
+    const headers = getHeaders({
+      'accept': 'application/vnd.github.loki-preview+json'
+    });
+    var transport;
+    var json;
+
+    before(function() {
+      transport = new Transport();
+      return transport.get(jsonUrl, headers)
+      .then(response => {
+        json = response;
+      });
+    });
+
+    it('Response should be a JS object', function() {
+      assert.typeOf(json, 'array');
+    });
+
+    it('Response should no be empty', function() {
+      assert.isAbove(json.length, 1);
+    });
+
+    it('Transport has latest headers', function() {
+      assert.typeOf(transport.latestHeaders, 'object');
+    });
+  });
+
+  describe('get() Buffer', () => {
+    const zipUrl = 'https://api.github.com/repos/mulesoft/api-console/zipball/v4.0.0';
+    const headers = getHeaders();
+    var transport;
+    var response;
+
+    before(function() {
+      this.timeout(20000);
+      transport = new Transport();
+      return transport.get(zipUrl, headers)
+      .then((res) => {
+        response = res;
+      });
+    });
+
+    it('Response should be a Buffer', function() {
+      assert.ok(response.buffer);
+    });
+
+    it('Response should no be empty', function() {
+      assert.isAbove(response.length, 1);
+    });
+  });
+});
